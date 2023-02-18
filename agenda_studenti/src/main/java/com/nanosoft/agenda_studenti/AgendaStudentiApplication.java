5fff90d1a94b0184b1dd522b383a83523b7aa668
FaolinZF <faolin92@gmail.com>
2023-02-18 11:57:44 +0100

    Inizializzazione progetto con springBootInizializer

---
+++ agenda_studenti/src/main/java/com/nanosoft/agenda_studenti/AgendaStudentiApplication.java
@@ -0,0 +1,13 @@
+package com.nanosoft.agenda_studenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaStudentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaStudentiApplication.class, args);
+	}
+
+}
---
+++ agenda_studenti/src/test/java/com/nanosoft/agenda_studenti/AgendaStudentiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanosoft.agenda_studenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaStudentiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
