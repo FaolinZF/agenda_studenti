42f73a18124d097afa5749c4e650abe29600bf58
FaolinZF <faolin92@gmail.com>
2023-02-20 18:55:57 +0100

    aggiornamento di AppuntamentiController per renderlo RESTfull e creazione classe di supporto AppuntamentiModelAssembler per creazione dei link
    aggiunta dipendenza al POM = spring-boot-starter-hateoas

--- src/main/java/com/nanosoft/agenda_studenti/AppuntamentiController.java
+++ src/main/java/com/nanosoft/agenda_studenti/AppuntamentiController.java
@@ -3,65 +3,86 @@ package com.nanosoft.agenda_studenti;
 import java.util.List;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.IanaLinkRelations;
 
 @RestController
 public class AppuntamentiController {
 
     private final AppuntamentiRepository repository;
+    private final AppuntamentiModelAssembler assembler;
 
-    AppuntamentiController(AppuntamentiRepository repository) {
+    AppuntamentiController(AppuntamentiRepository repository, AppuntamentiModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
     // tag::get-aggregate-root[]
     @GetMapping("/appuntamenti")
-    List<Appuntamenti> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Appuntamenti>> all() {
+
+        List<EntityModel<Appuntamenti>> appuntamenti = repository.findAll().stream().map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(appuntamenti, linkTo(methodOn(AppuntamentiController.class).all()).withSelfRel());
     }
     // end::get-aggregate-root[]
 
     // inserisci appuntamento
     @PostMapping("appuntamenti")
-    Appuntamenti newAppuntamenti(@RequestBody Appuntamenti newAppuntamenti) {
-        return repository.save(newAppuntamenti);
+    ResponseEntity<?> newAppuntamenti(@RequestBody Appuntamenti newAppuntamenti) {
+
+        EntityModel<Appuntamenti> entityModel = assembler.toModel(repository.save(newAppuntamenti));
+
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
     // appuntamenti per data
     @GetMapping("appuntamenti/data")
-    List<Appuntamenti> perData(@RequestParam LocalDate data) {
-        List<Appuntamenti> l = repository.findByDataOrderByOraAsc(data);
+    CollectionModel<EntityModel<Appuntamenti>> perData(@RequestParam LocalDate data) {
+
+        List<EntityModel<Appuntamenti>> l = repository.findByDataOrderByOraAsc(data).stream().map(assembler::toModel)
+                .collect(Collectors.toList());
         if (l.isEmpty()) {
             throw new AppuntamentoPerDataNotFoundException("Non ho trovato appuntamenti per il giorno " + data);
         }
-        return l;
+        return CollectionModel.of(l, linkTo(methodOn(AppuntamentiController.class).perData(data)).withSelfRel());
     }
 
     // appuntamenti per ufficio
     @GetMapping("appuntamenti/ufficio")
-    List<Appuntamenti> perTipoAppuntamento(@RequestParam TipoAppuntamento tipo) {
-        List<Appuntamenti> l = repository.findByTipoAppuntamentiOrderByDataAndOraAsc(tipo);
+    CollectionModel<EntityModel<Appuntamenti>> perTipoAppuntamento(@RequestParam TipoAppuntamento tipo) {
+
+        List<EntityModel<Appuntamenti>> l = repository.findByTipoAppuntamentiOrderByDataAndOraAsc(tipo).stream()
+                .map(assembler::toModel).collect(Collectors.toList());
         if (l.isEmpty()) {
             throw new AppuntamentoPerTipoAppuntamentiNotFoundException("Non ho trovato appuntamenti di tipo: " + tipo);
         }
-        return l;
+        return CollectionModel.of(l,
+                linkTo(methodOn(AppuntamentiController.class).perTipoAppuntamento(tipo)).withSelfRel());
     }
 
     // appuntamento per data e ora
     @GetMapping("appuntamenti/")
-    Appuntamenti appuntamentoSpecifico(@RequestParam LocalDate data, @RequestParam LocalTime ora) {
+    EntityModel<Appuntamenti> appuntamentoSpecifico(@RequestParam LocalDate data, @RequestParam LocalTime ora) {
+
         Appuntamenti a = repository.findByDataAndOra(data, ora);
         if (a == null) {
             throw new AppuntamentoPerDataOraNotFoundException(
                     "Non ho trovato l'appuntamento per il giorno " + data + " alle ore " + ora);
         }
-        return a;
+        return assembler.toModel(a);
     }
 
     @PutMapping("appuntamenti/put")
-    Appuntamenti replaceAppuntamentoAppuntamento(@RequestBody Appuntamenti newAppuntamento,
+    ResponseEntity<?> replaceAppuntamentoAppuntamento(@RequestBody Appuntamenti newAppuntamento,
             @RequestParam LocalDate data,
             @RequestParam LocalTime ora) {
 
@@ -73,10 +94,16 @@ public class AppuntamentiController {
             findedAppuntamento.setTipoAppuntamento(newAppuntamento.getTipoAppuntamento());
             findedAppuntamento.setUfficio(newAppuntamento.getUfficio());
             findedAppuntamento.setDescrizione(newAppuntamento.getDescrizione());
-            return repository.save(findedAppuntamento);
+
+            EntityModel<Appuntamenti> entityModel = assembler.toModel(repository.save(findedAppuntamento));
+
+            return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                    .body(entityModel);
         }
 
-        return newAppuntamenti(newAppuntamento);
+        EntityModel<Appuntamenti> entityModel = assembler.toModel(repository.save(newAppuntamento));
+
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
     @DeleteMapping("/appuntamenti/delete")
---
+++ src/main/java/com/nanosoft/agenda_studenti/AppuntamentiModelAssembler.java
@@ -0,0 +1,24 @@
+package com.nanosoft.agenda_studenti;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppuntamentiModelAssembler
+        implements RepresentationModelAssembler<Appuntamenti, EntityModel<Appuntamenti>> {
+
+    @Override
+    public EntityModel<Appuntamenti> toModel(Appuntamenti appuntamento) {
+
+        return EntityModel.of(appuntamento,
+                linkTo(methodOn(AppuntamentiController.class)
+                        .appuntamentoSpecifico(appuntamento.getData(), appuntamento.getOra())).withSelfRel(),
+                        linkTo(methodOn(AppuntamentiController.class).all()).withRel("appuntamenti"));
+
+    }
+
+}
